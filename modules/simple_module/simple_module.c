4372a6707a7acebdec316467909c59d9ec50a1bd
Patryk Duda <patryk.d@10g.pl>
2017-03-26 14:25:42 +0200

    CIRCULAR TODO: change chardev to miscdev

--- modules/simple_module/simple_module.c
+++ modules/simple_module/simple_module.c
@@ -13,18 +13,14 @@ MODULE_AUTHOR("Patryk Duda");
 #define MYBUF_SIZE 20
 #define CIRCULAR_MAJOR 199
 
-const char * const text = "SIMPLE. Read calls: %zu, Write calls: %zu\n";
-
-size_t read_count;
-size_t write_count;
 char *mybuf;
-bool copied;
+char *position; //To keep position between read calls
+static size_t tab_size;
+
 struct proc_dir_entry *proc_entry;
+static char proc_buff[10];
 
-/* Operations for /dev/circular */
 const struct file_operations circular_fops;
-
-/* Operations for /proc/circular */
 const struct file_operations proc_fops;
 
 
@@ -88,83 +84,67 @@ static void __exit circular_exit(void)
 ssize_t circular_read(struct file *filp, char __user *user_buf,
 	size_t count, loff_t *f_pos)
 {
-	size_t to_copy = strlen(mybuf);
+	size_t tab_left = tab_size - *f_pos;
+	size_t to_cpy = (count > tab_left) ? tab_left : count;
 
 	printk(KERN_WARNING "CIRCULAR: read f_pos is %lld\n", *f_pos);
-
-	if (*f_pos >= to_copy) {
-		return 0;
-	}
-
-	if (copy_to_user(user_buf, mybuf, to_copy)) {
+	
+	if( _copy_to_user(user_buf, position, to_cpy) != 0) {
 		printk(KERN_WARNING "CIRCULAR: could not copy data to user\n");
-		return -EFAULT;
+		return -EPERM;
 	}
-	read_count++;
-
-	*f_pos += to_copy;
-	return to_copy;
+		
+	*f_pos += to_cpy;
+	position += to_cpy;
+	return to_cpy;
 }
 
 ssize_t circular_write(struct file *filp, const char __user *user_buf,
 	size_t count, loff_t *f_pos)
 {
-	int written = 0;
-	int to_write = count;
+	size_t tab_left = tab_size - (position - mybuf);
+	size_t to_cpy = (count > tab_left) ? tab_left : count;
 	printk(KERN_WARNING "CIRCULAR: write f_pos is %lld\n", *f_pos);
 	
-	while (to_write > 0) {
-		if (*f_pos + count > MYBUF_SIZE - 1) {
-			count = MYBUF_SIZE - 1 - *f_pos;
-		}
-		if (copy_from_user(mybuf + *f_pos, user_buf, count)) {
-			printk(KERN_WARNING "CIRCULAR: could not copy data from user\n");
-			return -EFAULT;
-		}
-		to_write -= count;
-		written += count;
-		*f_pos = (*f_pos + count) % MYBUF_SIZE - 1;
+	if(tab_left <= 0) {
+		position = mybuf
+		return -EAGAIN;
 	}
-	write_count++;
-	
-	return written;
+
+	if( _copy_from_user(position, user_buf, to_cpy) != 0) {
+		printk(KERN_WARNING "CIRCULAR: could not copy data from user\n");
+		return -EFAULT;
+	}
+		
+	*f_pos += to_cpy;
+	position += to_cpy;
+	return to_cpy;
 }
 
-ssize_t circular_read_proc(struct file *filp, char *user_buf,
-	size_t count, loff_t *f_pos)
+static ssize_t circular_write_proc(struct file *file, const char __user 
+	*buffer, size_t length, loff_t *offset)
 {
-	char *buf;
-	size_t length;
-	ssize_t retval = 0;
-
-	buf = kmalloc(100, GFP_KERNEL);
-	if (!buf) {
-		retval = -ENOMEM;
-		goto out;
+	size_t new_tab_size;
+	char *tmp_tab;
+	
+	if(length >= 10) return -ENOSPC;
+	if( _copy_from_user(proc_buff, buffer, length) != 0) return -EFAULT;
+		
+	sscanf(proc_buff, "%u", &new_tab_size);
+	
+	printk(KERN_INFO "CIRCULAR: new_tab_size %d bytes\n", new_tab_size);
+	
+	if((tmp_tab = krealloc(mybuf, new_tab_size, GFP_KERNEL)) == NULL) {
+		printk(KERN_ALERT "CIRCULAR: can't get ram\n");
+		return -ENOMEM;
 	}
+	
+	tab_size = ksize(tmp_tab); //Niekoniecznie moglismy dostac tyle ile chcielismy
+	mybuf = tmp_tab;
 
-	if (!copied) {
-		length = snprintf(buf, 100, text, read_count, write_count);
-		if (count < length) {
-			retval = -EFBIG;
-			goto out;
-		}
+	printk(KERN_INFO "CIRCULAR: allocated %d bytes of memory\n", tab_size);
 
-		if (copy_to_user(user_buf, buf, length)) {
-			printk(KERN_WARNING "CIRCULAR: could not copy data to user\n");
-			retval = -EFAULT;
-			goto out;
-		}
-		retval = count;
-		copied = true;
-	} else {
-		retval = 0;
-		copied = false;
-	}
-
-out:
-	kfree(buf);
-	return retval;
+	return length;
 }
 
 const struct file_operations circular_fops = {
@@ -173,7 +153,7 @@ const struct file_operations circular_fops = {
 };
 
 const struct file_operations proc_fops = {
-	.read = circular_read_proc,
+	.write = circular_write_proc,
 };
 
 module_init(circular_init);
